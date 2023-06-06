931108ee2d9108602fae70257b194557397f9ff4
Kimiko00 <yota.priyanto@gmail.com>
2023-06-06 22:37:48 +0700

    change the config for twitter controller

--- server/controllers/tweet.controller.js
+++ server/controllers/tweet.controller.js
@@ -1,51 +1,70 @@
 const TweetAPI = require("../api/twitter.api");
 
-async function TWEET({ query, limit }) {
-  const n = await TweetAPI.get(
-    `/2/tweets/search/recent?query=${query}&max_results=${limit}&tweet.fields=author_id&expansions=attachments.media_keys&media.fields=url`
-  )
-    .then((res) => {
-      // loop kedalam semua data di include.media
-      // kemudian simpan sebagai dict baru
-      var includeMap = {};
-      var includeLoop = res.data.includes
-        ? res.data.includes.media.length > 0
-          ? res.data.includes.media.forEach((incl) => {
-              if (!incl.hasOwnProperty(incl.media_key))
-                includeMap[incl.media_key] = incl;
-            })
-          : ""
-        : " ";
-      // console.log("include map: ", includeMap);
+// NEED TO HANDLE ERROR IF THE USER ADD # SPACE OR EVERY UNIQ PARAM IN QUERY
+// RETURN ERROR "PLEASE DON'T INSERT ANY UNIQ CHAR IN YOUR INPUT"
+async function TWEET({ query }) {
+  // change the inputed query into hastage search
+  const hashtag = encodeURIComponent(`#${query}`);
 
-      // checking by the media_keys
-      // if have the same keys then append
-      var b = res.data.data.map((x) => {
-        var singleAttachment = [];
+  const tweetPost = await TweetAPI.get(
+    `/2/tweets/search/recent?query=${hashtag}&max_results=15&tweet.fields=author_id,created_at,entities&expansions=attachments.media_keys&media.fields=url`
+  ).then((res) => {
+    return res.data;
+  });
+  const tweetData = tweetPost.data.map((datas) => {
+    // change the timestamp to date
+    const dateString = datas.created_at;
+    const date = new Date(dateString);
 
-        if (x.attachments)
-          x.attachments.media_keys.forEach((media_q) => {
-            if (includeMap.hasOwnProperty(media_q))
-              singleAttachment.push(includeMap[media_q]);
-          });
+    return {
+      id: datas.author_id,
+      text: datas.text.replace(/https?:\/\/[^\s]+/g, "").replace(/\n/g, ""),
+      create_at: date.toDateString(),
 
-        return {
-          author_id: x.author_id,
-          text: x.text,
-          attach:
-            !x.attachments || !singleAttachment ? undefined : singleAttachment,
-        };
-      });
-      return { results: b, err: null };
-    })
-    .catch((e) => {
-      console.log("err : ", e);
-      return {
-        results: null,
-        err: e.message,
-      };
-    });
-  return n.err ? n.err : n.results;
+      // checking if there any entities inculeded in the response json
+      // then checking if there any url included
+      ...(datas.entities
+        ? datas.entities.urls && datas.entities.urls.length > 0
+          ? {
+              // use the last url for the link then the rest if any
+              link:
+                datas.entities.urls[3] ||
+                datas.entities.urls[2] ||
+                datas.entities.urls[1] ||
+                datas.entities.urls[0],
+            }
+          : {}
+        : {}),
+    };
+  });
+  const authorID = tweetData.map((items) => items.id).join(",");
+  // console.log(authorID);
+
+  const userID = await TweetAPI.get(
+    `/2/users?ids=${authorID}&user.fields=profile_image_url`
+  ).then((res) => {
+    return res.data;
+  });
+
+  const getID = userID.data.map((id) => {
+    return {
+      id: id.id,
+      name: id.name,
+      username: id.username,
+      profile: id.profile_image_url,
+    };
+  });
+  // combine 2 json from request using id
+  // to get the user id and username from different api
+  const mergeData = tweetData.map((id1) => {
+    const id2 = getID.find((id2) => id2.id === id1.id);
+    return {
+      ...id1,
+      ...id2,
+    };
+  });
+
+  return mergeData;
 }
 
 module.exports = TWEET;
--- server/controllers/tweetUser.controller.js
+++ server/controllers/tweetUser.controller.js
@@ -1,25 +1,70 @@
 const TweetAPI = require("../api/twitter.api");
 
+// NEED TO ADD HANDLER FOR ERROR FUNCTION
+// IF THE DATA IS NOT SHOWING (403 AUTH ERROR FROM TWITTER)
 async function TWEETUSER({ query }) {
-  const userID = await TweetAPI.get(
-    `https://api.twitter.com/2/users/by?usernames=${query}`
-  ).then((res) => {
-    return res.data;
-  });
-  var getID = userID.data.map((id) => {
-    return id.id;
-  });
-  var user_id = getID.toString();
+  try {
+    const userID = await TweetAPI.get(
+      `/2/users/by?usernames=${query}&user.fields=name,profile_image_url,url,username`
+    ).then((res) => {
+      return res.data;
+    });
 
-  const userTimeline = await TweetAPI.get(
-    `https://api.twitter.com/2/users/${user_id}/tweets?max_results=15`
-  ).then((response) => {
-    return response.data;
-  });
-  const userDataTimeline = userTimeline.data.map((userDataTimelines) => {
-    return userDataTimelines;
-  });
-  return userDataTimeline;
+    const getID = userID.data.map((id) => {
+      return {
+        id: id.id,
+        name: id.name,
+        username: id.username,
+        profile: id.profile_image_url,
+      };
+    });
+
+    // pick the user id from response json
+    const user_id = getID[0].id;
+
+    const userTimeline = await TweetAPI.get(
+      `/2/users/${user_id}/tweets?max_results=15&tweet.fields=created_at,entities,source`
+    ).then((response) => {
+      return response.data;
+    });
+
+    const userDataTimeline = userTimeline.data.map((userDataTimelines) => {
+      // change the timestamp to date
+      const dateString = userDataTimelines.created_at;
+      const date = new Date(dateString);
+
+      return {
+        text: userDataTimelines.text
+          .replace(/https?:\/\/[^\s]+/g, "")
+          .replace(/\n/g, ""),
+        create_at: date.toDateString(),
+
+        // checking if there any entities inculeded in the response json
+        // then checking if there any url included
+        ...(userDataTimelines.entities
+          ? userDataTimelines.entities.urls &&
+            userDataTimelines.entities.urls.length > 0
+            ? {
+                // use the last url for the link then the rest if any
+                link:
+                  userDataTimelines.entities.urls[3] ||
+                  userDataTimelines.entities.urls[2] ||
+                  userDataTimelines.entities.urls[1] ||
+                  userDataTimelines.entities.urls[0],
+              }
+            : {}
+          : {}),
+      };
+    });
+    const result = {
+      user: getID,
+      feed: userDataTimeline,
+    };
+    return result;
+  } catch (error) {
+    // console.error("Error:", error.message);
+    return "404 not Found";
+  }
 }
 
 module.exports = TWEETUSER;
